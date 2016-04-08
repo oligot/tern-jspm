67a14004f5ad3118e1371cd9934b758bf61213cf
Olivier Ligot <olivier.ligot@groups.be>
2016-04-08 14:53:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const reqFrom = require('req-from');
+const reqCwd = require('req-cwd');
+const jspm = reqCwd('jspm');
+
+var infer;
+var cache = {};
+var loader;
+
+function resolve(name, parentFile) {
+  if (name.substr(0, 2) == './') {
+    name = path.resolve(path.dirname(parentFile), name);
+  }
+  var resolved = loader.normalizeSync(name).replace("file://", "");
+  if (resolved) {
+    try {
+      if (fs.statSync(resolved).isDirectory()) {
+        resolved += '.js';
+      }
+    } catch(e) {
+    }
+  }
+  resolved = resolved && infer.cx().parent.normalizeFilename(resolved);
+  console.log(name, parentFile, resolved);
+  if (name.substr(0, 4) == 'npm:') {
+    cache[parentFile] = resolved;
+  } else {
+    resolved = cache[resolved] || resolved;
+  }
+  return resolved;
+}
+
+exports.initialize = function(ternDir) {
+  jspm.setPackagePath('.');
+  loader = jspm.Loader();
+  reqFrom(path.resolve(ternDir, 'plugin'), './es_modules');
+  reqFrom(path.resolve(ternDir, 'plugin'), './node_resolve');
+  infer = reqFrom(path.resolve(ternDir, 'lib'), './infer');
+  const tern = reqFrom(path.resolve(ternDir, 'lib'), './tern');
+  tern.registerPlugin('jspm', function(server) {
+    server.loadPlugin('es_modules');
+    server.loadPlugin('node_resolve');
+    server.mod.modules.resolvers.push(resolve);
+  })
+};
---
+++ readme.md
@@ -0,0 +1,34 @@
+# tern-jspm
+
+> [Tern](http://ternjs.net/) plugin to support the [Jspm package manager](http://jspm.io/)
+
+## Installation
+
+Install with npm
+
+```shell
+npm install --save-dev tern-jspm
+```
+
+## Usage
+
+Add the plugin to the tern configuration file _.tern-project_
+```json
+{
+  "plugins": {
+    "jspm": {}
+  }
+}
+```
+
+Relaunch your editor.
+You should now be able to jump to your modules and dependencies with ease :smirk:
+
+## Credits
+
+Credits goes to the NFL team for their great work on [jspm-resolve](https://github.com/nfl/jspm-resolve).
+
+
+## License
+
+MIT © [Olivier Ligot](http://www.oligot.be/)
